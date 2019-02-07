b2fcf913c6cce2d7a85ecabe226f1e7447a10c3e
ryner <47404527+rynergalaus@users.noreply.github.com>
2019-02-07 11:00:11 +0800

    retest link

--- rsg/main-callback.php
+++ rsg/main-callback.php
@@ -3,27 +3,35 @@
 	<h1 style="text-align:center; font-size:30px; color:#fff;" >COMPILATION OF ADDONS</h1><br>
 	Active Addons / Plugins
 	<div style="padding:10px 20px;">
-		<?php
-			if(class_exists('RSGLTA')){
-				echo '<a href="'.get_home_url().'/wp-admin/admin.php?page=rsglta"><strong>RSG Libraries Theme Addon</strong></a>';
-				echo '<div style="padding:5px 20px;">
-						Includes common libraries usable for your theme.
-					 </div><br>';
-			}
-			if(class_exists('RSGCAD')){
-				echo '<a href="'.get_home_url().'/wp-admin/admin.php?page=rsgcad"><strong>RSG Countries Array for Developers</strong></a>';
-				echo '<div style="padding:5px 20px;">
-						List of all countries and their respective states / provinces / regions / municipalities.<br>
-						List update as of February 2019.
-					 </div><br>';
-			}
-			if(class_exists('RSGGDS')){
-				echo '<a href="'.get_home_url().'/wp-admin/admin.php?page=rsggds"><strong>RSG Retrieve Google Drive Spreadsheet</strong></a>';
-				echo '<div style="padding:5px 20px;">
-						Retrieve contents from google drive spreadsheet and display it as a table.<br>
-						Uses the <a href="https://mottie.github.io/tablesorter/docs/">tablesorter script library</a>.
-					 </div><br>';
-			}
-		?>
+
+		<div class="RSGLTA">
+			<a href="/wp-admin/admin.php?page=rsglta">
+				<strong>RSG Libraries Theme Addon</strong>
+			</a>
+			<div style="padding:5px 20px;">
+				Includes common libraries usable for your theme.
+			</div>
+		</div>
+
+		<div class="RSGCAD">
+			<a href="/wp-admin/admin.php?page=rsgcad">
+				<strong>RSG Countries Array for Developers</strong>
+			</a>
+			<div style="padding:5px 20px;">
+				List of all countries and their respective states / provinces / regions / municipalities.<br>
+				List update as of February 2019.
+			</div>
+		</div>
+
+		<div class="RSGGDS">
+			<a href="/wp-admin/admin.php?page=rsggds">
+				<strong>RSG Retrieve Google Drive Spreadsheet</strong>
+			</a>
+			<div style="padding:5px 20px;">
+				Retrieve contents from google drive spreadsheet and display it as a table.<br>
+				Uses the <a href="https://mottie.github.io/tablesorter/docs/">tablesorter script library</a>.
+			</div>
+		</div>
+
 	</div>
 </div>
